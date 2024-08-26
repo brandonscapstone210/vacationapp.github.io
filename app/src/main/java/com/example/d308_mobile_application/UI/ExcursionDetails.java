b048269ed29b9d5f905b262a2e3cdfdc692232e7
Brandon Denning <brdenning3@gmail.com>
2024-08-25 21:22:56 -0600

    Added ExcursionDetails.java and layout page.

--- app/src/main/java/com/example/d308_mobile_application/UI/ExcursionDetails.java
+++ app/src/main/java/com/example/d308_mobile_application/UI/ExcursionDetails.java
@@ -1,6 +1,5 @@
 package com.example.d308_mobile_application.UI;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -10,20 +9,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.d308_mobile_application.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class VacationDetails extends AppCompatActivity {
+public class ExcursionDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_vacation_details);
-        FloatingActionButton fab=findViewById(R.id.floatingActionButton2);
-        fab.setOnClickListener(view -> {
-            Intent intent = new Intent(VacationDetails.this, ExcursionDetails.class);
-            startActivity(intent);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_excursion_details);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
-
-
     }
 }
