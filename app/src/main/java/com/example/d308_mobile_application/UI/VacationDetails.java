8211052fe7998c7f2b3d2b1e4e581885dc32af55
Brandon Denning <brdenning3@gmail.com>
2024-08-26 09:19:57 -0600

    Built DatePicker

--- app/src/main/java/com/example/d308_mobile_application/UI/VacationDetails.java
+++ app/src/main/java/com/example/d308_mobile_application/UI/VacationDetails.java
@@ -1,7 +1,11 @@
 package com.example.d308_mobile_application.UI;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +16,16 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.d308_mobile_application.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 public class VacationDetails extends AppCompatActivity {
+    Button button;
+    DatePickerDialog.OnDateSetListener myDate;
+    final Calendar myCalendar = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +37,43 @@ public class VacationDetails extends AppCompatActivity {
             startActivity(intent);
         });
 
+        button = findViewById(R.id.datePicker);
+        String myFormat = "MM/dd/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        String currentDate = sdf.format(new Date());
+        button.setText(currentDate);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Date date;
+                String info = button.getText().toString();
+                try {
+                    myCalendar.setTime(sdf.parse(info));
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                new DatePickerDialog(VacationDetails.this, myDate, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+
+        myDate = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int monthOfYear, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
 
     }
+
+
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        button.setText(sdf.format(myCalendar.getTime()));
+    }
+
 }
