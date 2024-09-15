675674c5a36ec48fc95e112ccdf9be8c7927cf9b
Brandon Denning <brdenning3@gmail.com>
2024-09-15 14:17:50 -0600

    Searchview: searchadapter created, added to main activity. added search_view xml.

--- app/src/main/java/com/example/d308_mobile_application/UI/MainActivity.java
+++ app/src/main/java/com/example/d308_mobile_application/UI/MainActivity.java
@@ -16,23 +16,36 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.lifecycle.LiveData;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.room.Query;
 
 import com.example.d308_mobile_application.R;
 import com.example.d308_mobile_application.database.Repository;
+import com.example.d308_mobile_application.entities.Excursion;
 import com.example.d308_mobile_application.entities.Vacation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
     public static int numAlert;
+    private Repository repository;
+    List<Vacation> allVacations;
+    List<Excursion> allExcursions;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         Button button=findViewById(R.id.button);
+
+        repository = new Repository(getApplication());
+
         //below are the actions to take us to the next screen using intent.
         button.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -43,7 +56,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        SearchView searchView = findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener(){
 
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                try{
+                    allVacations = repository.getAllVacations();
+                    allExcursions = repository.getAllExcursions();
+                } catch (ExecutionException | InterruptedException e) {
+                    throw  new RuntimeException(e);
+                }
+
+                List<String> vacationNames = allVacations.stream().map(Vacation::getVacationName).collect(Collectors.toList());
+                List<String> excursionNames = allExcursions.stream().map(Excursion::getExcursionName).collect(Collectors.toList());
+
+                List<String> allSearch = new ArrayList<>();
+                allSearch.addAll(vacationNames);
+                allSearch.addAll(excursionNames);
+
+                List<String> filteredList = allSearch.stream().filter(item -> item.toLowerCase().contains(newText.toLowerCase())).collect(Collectors.toList());
+
+                RecyclerView recyclerView = findViewById(R.id.searchRecyclerView);
+
+
+                return false;
+            }
+        });
 
     }
 }
